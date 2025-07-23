00a62318c80b74dbc926765bee81ec991d2bbe5a
Gonzalo Olave <growolff@gmail.com>
2025-07-23 00:51:12 -0400

    add base sketch and libraries

---
+++ iroh_base/iroh_base.ino
@@ -0,0 +1,15 @@
+#include <KnightRoboticsLibs_Iroh.h>
+#include <LiquidCrystal_I2C.h>
+#include <NewPing.h>
+#include <Servo.h>
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
